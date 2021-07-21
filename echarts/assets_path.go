bbed7becf2513a5671022723049744e82147a9d0
916anan <40131953+916anan@users.noreply.github.com>
2021-07-21 11:20:11 +0800

    [新增功能](master): 初次提交修改引用

---
+++ echarts/assets_path.go
@@ -0,0 +1,9 @@
+package echarts
+
+var AssetPaths = map[string]string{
+	"china.js":"/china.js",
+	"echarts-gl.min.js":"/echarts-gl.min.js",
+	"echarts-liquidfill.min.js":"/echarts-liquidfill.min.js",
+	"echarts.min.js":"/echarts.min.js",
+
+}
---
+++ login/theme1/assets_path.go
@@ -0,0 +1,7 @@
+package theme1
+
+var AssetPaths = map[string]string{
+	"all.min.css":    "/login/dist/all.min.css",
+	"all.min.js":     "/login/dist/all.min.js",
+	"respond.min.js": "/login/dist/respond.min.js",
+}
